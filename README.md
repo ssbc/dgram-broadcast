fdbca4509d281f1f44d62984cf1f5a8f133bf677
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-19 19:01:05 +0200

    initial

---
+++ README.md
@@ -0,0 +1,49 @@
+# broadcast-stream
+
+A more obvious dgram broadcast.
+
+broadcast stream has the correct defaults to just work
+for broadcast on your local network.
+
+## Example
+
+broadcast on a port on your local network.
+
+``` js
+var createStream = require('broadcast-stream')
+
+var stream = createStream(8999)
+
+stream.on('data', function (msg) {
+  console.log(msg.toString())
+  console.log(msg.address, msg.port, msg.echo)
+})
+
+setInterval(function () {
+  stream.write(new Date())
+}, 1000)
+
+```
+
+## api
+
+### stream = createStream (port, loopback=true)
+
+stream on the `port`. If `loopback` is false, do not output your own messages.
+returns a stream.
+
+### stream.on('data', msg)
+
+`msg` is the message as a buffer,
+`msg` also has `msg.address` and `msg.port` set to the address and port that originated the message.
+if msg is a message that this stream sent, it will also have `msg.loopback = true`.
+
+### back-pressure
+
+udp does not have back-pressure, but the stream api does.
+if the stream is paused, it will just drop messages.
+if when the stream is resumed, it will emit the latest message.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,76 @@
+
+var udp = require('dgram')
+var pipe = require('stream').prototype.pipe
+var os = require('os')
+
+module.exports = function (port, loopback) {
+
+  var addresses = {}
+  var socket = udp.createSocket('udp4')
+
+  socket.readable = socket.writable = true
+
+  socket.write = function (message) {
+    if('string' === typeof message)
+      message = new Buffer(message, 'utf8')
+    socket.send(message, 0, message.length, port, '255.255.255.255')
+    return true
+  }
+
+  socket.end = function () {
+    socket.close()
+  }
+
+  socket.on('close', function () {
+    socket.emit('end')
+  })
+
+  var latest = null
+
+  socket.on('message', function (msg, other) {
+    if(addresses[other.address] && other.port === port) {
+      if(loopback === false) return
+      msg.loopback = true
+    }
+
+    msg.port = other.port
+    msg.address = other.address
+
+    //if paused, remember the latest item.
+    //otherwise just drop those messages.
+    if(socket.paused)
+      return latest = msg
+
+    latest = null
+    socket.emit('data', msg)
+  })
+
+  socket.pause = function () {
+    socket.paused = true
+    return this
+  }
+
+  socket.resume = function () {
+    socket.paused = false
+    if(latest) {
+      var msg = latest
+      latest = null
+      socket.emit('data', msg)
+    }
+    return this
+  }
+
+  socket.bind(port)
+  socket.on('listening', function () {
+    var ifaces = os.networkInterfaces()
+    for(var k in ifaces)
+      ifaces[k].forEach(function (address) {
+        addresses[address.address] = true
+      })
+    socket.setBroadcast(true)
+  })
+
+  socket.pipe = pipe
+
+  return socket
+}
