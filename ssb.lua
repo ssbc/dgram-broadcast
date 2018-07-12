2cc39b89d4a17c92675b45d62671a5637acdef19
Jonathan Dahan <hi@jonathan.is>
2018-07-12 13:55:09 -0400

    add wireshark decoder

---
+++ ssb.lua
@@ -0,0 +1,47 @@
+-- [@micro](@wsoeSOhEE3yAIEF1gTVMeStUWnoejIz1P6a3lnvjaOM=.ed25519)'s very hacky ssb broadcast protocol decoder
+-- cp ssb.lua ~/.config/wireshark/plugins/
+local ssb_protocol = Proto("ssb", "Scuttlebutt");
+
+local f_ip = ProtoField.string("ssb.ip")
+local f_port = ProtoField.string("ssb.port")
+local f_public_key = ProtoField.string("ssb.public_key")
+
+ssb_protocol.fields = { f_ip, f_port, f_public_key }
+
+local data_dis = Dissector.get("data")
+
+function string:split(sep)
+   local sep, fields = sep or ":", {}
+   local pattern = string.format("([^%s]+)", sep)
+   self:gsub(pattern, function(c) fields[#fields+1] = c end)
+   return fields
+end
+
+function ssb_protocol.dissector(buffer, pinfo, tree)
+    length = buffer:len()
+    if length == 0 then return end
+
+    pinfo.cols.protocol = ssb_protocol.name
+
+    local subtree = tree:add(ssb_protocol, buffer())
+    local parts = buffer():string():split()
+    local net = parts[1]
+    local ip = parts[2]
+    local shs = parts[3]
+    local publickey = parts[4]
+
+    local port = parts[3]:split("~")[1]
+    local length = 1
+    length = length + net:len()
+
+    subtree:add(f_ip, buffer(length, ip:len()))
+    length = length + ip:len() + 1
+
+    subtree:add(f_port, buffer(length, port:len()))
+    length = length + shs:len() + 1
+
+    subtree:add(f_public_key, buffer(length, publickey:len()))
+end
+
+local udp_encap_table = DissectorTable.get("udp.port")
+udp_encap_table:add(8008, ssb_protocol)
