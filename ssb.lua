81c8ae3af3dd2d0c05752521d2151e1961f00744
Jonathan Dahan <hi@jonathan.is>
2018-07-15 10:31:59 -0400

    parse multiserver format correctly, see multiserver readme

--- ssb.lua
+++ ssb.lua
@@ -14,7 +14,7 @@ function string:split(sep)
    local sep, fields = sep or ":", {}
    local pattern = string.format("([^%s]+)", sep)
    self:gsub(pattern, function(c) fields[#fields+1] = c end)
-   return fields
+   return table.unpack(fields)
 end
 
 function ssb_protocol.dissector(buffer, pinfo, tree)
@@ -24,25 +24,22 @@ function ssb_protocol.dissector(buffer, pinfo, tree)
     pinfo.cols.protocol = ssb_protocol.name
 
     local subtree = tree:add(ssb_protocol, buffer())
-    local parts = buffer():string():split()
-    local net = parts[1]
-    local ip = parts[2]
-    local shs = parts[3]
-    local publickey = parts[4]
+    local net, shs = buffer():string():split("~")
+    local proto, ip, port = net:split()
+    local prefix, key = shs:split()
 
-    local port = parts[3]:split("~")[1]
-    local length = 1
-    length = length + net:len()
+    local length = proto:len() + 1
 
     subtree:add(f_ip, buffer(length, ip:len()))
     length = length + ip:len() + 1
 
     subtree:add(f_port, buffer(length, port:len()))
-    length = length + shs:len() + 1
+    length = length + port:len() + 1
+    length = length + prefix:len() + 1
 
-    subtree:add(f_key, buffer(length, publickey:len()))
+    subtree:add(f_key, buffer(length, key:len()))
 
-    subtree:append_text(", IP: " .. ip .. ", Port: " .. port .. ", Key: " .. publickey)
+    subtree:append_text(", IP: " .. ip .. ", Port: " .. port .. ", Key: " .. key)
 end
 
 local udp_encap_table = DissectorTable.get("udp.port")
